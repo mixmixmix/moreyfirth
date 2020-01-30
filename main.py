26948a07192cd8cdffdb08b7a4f239d619c6b3fe
mixmixmix <mikolaj.kundegorski@gmail.com>
2020-01-30 21:43:06 +0000

    produces correct graphs per fish

--- main.py
+++ main.py
@@ -4,12 +4,15 @@ import glob
 
 filtered_file = 'data/filtered.csv'
 fish_file = 'data/fish.csv'
+tag_file = 'data/moray_tag_info.csv'
 
 salmomiks.load_data('data/recievers.csv'
-          , glob.glob('data/observations/*csv')
-          , filtered_file
+                    , tag_file
+                    , glob.glob('data/observations/*csv')
+                    , filtered_file
 )
 
 salmomiks.make_rec_map(filtered_file, 'data/recmap.html')
+
 
 salmomiks.make_fish_df(filtered_file, fish_file)
--- salmomiks.py
+++ salmomiks.py
@@ -1,13 +1,16 @@
 
+import glob
 import csv
 import pandas as pd
-import folium
-import glob
 import numpy as np
 import matplotlib.pyplot as plt
+import seaborn as sns
+import dateutil
 import folium
+import pyproj
 
 def load_data(recievers_list_file
+              , tag_list_file
               , observations_filelist
               , filtered_data_file
 ):
@@ -15,9 +18,15 @@ def load_data(recievers_list_file
     #TODO make sure correct results on different systems
     print('Processing receiver list. This is for NESS system only for now')
     receiver_list = pd.read_csv(recievers_list_file)
+    print('Processing tag list. This is for NESS system only for now')
+    tag_list = pd.read_csv(tag_list_file)
+    tag_list = pd.read_csv(tag_list_file)
+    tag_list = tag_list[tag_list['River']=='Ness']['Tag_ID'].reset_index(drop=True) #TODO add predator tag info
     print('Removing records with invalid receiver numbers')
     print(receiver_list[receiver_list['id'].isnull()])
     receiver_list = receiver_list.dropna()
+
+
 
     if len(receiver_list['id']) != len(receiver_list['id'].unique()):
         print('WTF! Multiple receivers with the same ID! Will use the top value of GPS')
@@ -34,7 +43,13 @@ def load_data(recievers_list_file
         df = pd.read_csv(f)
         df['rec_id']=rec_id
         print(rec_id)
+
         select_filter = receiver_list['id']==rec_id
+
+        #60/66 fix?HACK???
+        #if rec_id == 483466:
+        #    select_filter = receiver_list['id']==483460
+
         if (select_filter).any():
             df['lat']=receiver_list[select_filter]['lat'].iloc[0]
             df['lon']=receiver_list[select_filter]['lon'].iloc[0]
@@ -65,8 +80,45 @@ def load_data(recievers_list_file
 
     #check for the same fish in multiple systems, optional #TODO
 
+    #Remove alien tags
+    print('Removing records with invalid tag numbers')
+    df_observations['tags'] = df_observations.apply(lambda x: int(x['transmitter'].split('-')[-1]),axis=1)
 
+    tags_in_system = tag_list.values
+    transmitters_in_system = df_observations['tags'].unique()
+    #print(tags_in_system)
+    #print(transmitters_in_system)
+    print('Removing {} alien signals'.format(len(list(set(transmitters_in_system)-set(tags_in_system)))))
+    df_observations = df_observations[df_observations['tags'].isin(tag_list.values)]
 
+    print('Calculate distances')
+    print(df_observations.sample(30))
+    #Adding distance from first reciever
+    first_rec_id=480432
+    rec_list = df_observations.drop_duplicates(['rec_id']).set_index('rec_id')
+    rec_list = rec_list.dropna()
+    print(rec_list)
+    geod = pyproj.Geod(ellps='WGS84')
+    dists=dict()
+    for index, rec in rec_list.iterrows():
+        if index == first_rec_id:
+            dist = 0
+        else:
+            _, _,dist = geod.inv(  rec_list.loc[first_rec_id]['lon']
+                                , rec_list.loc[first_rec_id]['lat']
+                                , rec['lon']
+                                , rec['lat'])
+        dists[index]=int(dist)
+
+    rec_list['dist_first']=pd.Series(dists)
+    df_observations['dist_first'] = df_observations['rec_id'].replace(dists)
+    df_observations['epoch'] = df_observations.apply (lambda row: dateutil.parser.isoparse(row['datetime']).timestamp(), axis=1)
+
+    #Adding time in 0.1 of hours
+    epoch_beginning = 1552413619.0
+    df_observations['epoch'] = df_observations.apply (lambda row: ((row['epoch']-epoch_beginning)/360), axis=1) #epoch is the time in 0.1 of hours from the first ping ever on NESS
+
+    df_observations = df_observations.sort_values(by=['epoch']).reset_index(drop=True)
 
     print('Saving observations to filtered.csv')
     df_observations.to_csv(filtered_data_file)
@@ -98,5 +150,76 @@ def make_rec_map(filtered_file, map_file):
         ).add_to(m)
         m.save(map_file)
 
+#This function is NESS specific for now (TODO)
 def make_fish_df(filtered_file, fish_file):
-    filtered_list = pd.read_csv(filtered_file)
+    #first receiver id
+    filtered_list = pd.read_csv(filtered_file).dropna()
+    filtered_list = filtered_list.sort_values(by=['epoch']).reset_index(drop=True)
+
+    transmitters = filtered_list['transmitter'].unique()
+    print('Printing graphs...')
+
+    for tx in transmitters:
+        ff = filtered_list[filtered_list['transmitter']==tx].reset_index(drop=True)
+        if(len(ff)<10):
+            continue
+        ax = sns.countplot(x="dist_first", data=ff)
+        ax.set_xticklabels(ax.get_xticklabels(),rotation=30)
+        ax.set_title(str(tx))
+        plt.savefig('data/output/distcount'+str(tx)+'png')
+        plt.clf()
+        #ay = sns.distplot(ff['epoch'].values)
+        ay = sns.scatterplot(x="epoch", y='dist_first', data=ff)
+        #ay.set_xticklabels(ay.get_xticklabels(),rotation=90)
+        ay.set_title(str(tx))
+        plt.savefig('data/output/timedist'+str(tx)+'png')
+        plt.clf()
+
+    print("done!")
+
+#import dateutil
+#timethreshold = 30 #how many seconds max difference between pings from a fish
+#dffilts = dict()
+#for key, df in dfs.items():
+#    df['tesc'] = df.apply (lambda row: dateutil.parser.isoparse(row['Date and Time (UTC)']).timestamp(), axis=1)
+#    df['datetime'] = df.apply (lambda row: dateutil.parser.isoparse(row['Date and Time (UTC)']), axis=1)
+#    df = df [['tesc','datetime','Transmitter']]
+#    #df.set_index('tesc')
+#    df['fishdiff']= ~(df['Transmitter'] == df['Transmitter'].shift())
+#    df['mfishdiff']= ~(df['Transmitter'].shift(-1) == df['Transmitter'])
+#    df['tdiff']=df['tesc'] - df['tesc'].shift(1, fill_value=1554901439)
+#    df['mtdiff']=df['tesc'].shift(-1, fill_value=1554901439) - df['tesc']
+#    dffilt = df[((df['tdiff']<timethreshold) & (df['fishdiff'])) | ((df['mtdiff']<timethreshold)  & (df['mfishdiff']))]
+#    plt.plot(dffilt['datetime'],dffilt['tdiff'],'o')
+#    plt.show()
+#    display(dffilt.head(10))
+#    dffilts[key]=dffilt
+#
+#In [ ]:
+##give clusters a name
+#for dffilt in dffilts.values():
+#    groupid = 0
+#    shoals = []
+#    for ix, valx in dffilt.iterrows():
+#        if valx['tdiff']>=timethreshold:
+#            groupid=groupid+1
+#        shoals.append(groupid)
+#        
+#    dffilt['shoals']=shoals        
+#    plt.plot(dffilt['datetime'],dffilt['shoals'],'o')
+#    plt.show()
+#
+##plot cluster sizes x cluster number per time
+##show that on a map
+#
+#In [ ]:
+#for key, dffilt in dffilts.items():
+#    print(key)
+#    plt.plot(dffilt.groupby('shoals').size())
+#    plt.show()
+#
+#In [ ]:
+#
+#
+#
+#
