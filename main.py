7d6583485079bdb893fdde1a05b5b1a5e1f4a0c4
mixmixmix <mikolaj.kundegorski@gmail.com>
2020-02-17 13:31:48 +0000

    find pairs in the data

--- main.py
+++ main.py
@@ -3,21 +3,31 @@ import salmomiks
 import glob
 
 filtered_file = 'data/filtered.csv'
-fish_file = 'data/fish.csv'
 tag_file = 'data/moray_tag_info.csv'
+rec_file_better = 'data/moray_tag_info_more.csv'
 fish_file = 'data/fish_moove.csv'
 rec_groups_dir = 'data/rec_groups/'
+main_map_file ='data/recmap.html'
+pairs_map_file ='data/pairs_recmap.html'
 
 #salmomiks.load_data('data/recievers.csv'
 #                    , tag_file
 #                    , glob.glob('data/observations/*csv')
 #                    , filtered_file
+#                    , rec_file_better
 #)
-#
-#salmomiks.make_rec_map(filtered_file, 'data/recmap.html')
+
+#salmomiks.make_rec_map(filtered_file, main_map_file)
 #
 #
 #salmomiks.make_fish_corr(filtered_file)
 #salmomiks.make_fish_dist(filtered_file)
-salmomiks.make_fish_speed(filtered_file, fish_file)
-salmomiks.check_fish_coocurrence(fish_file, rec_groups_dir)
+#salmomiks.make_fish_speed(filtered_file, fish_file)
+#salmomiks.check_fish_coocurrence(fish_file, rec_groups_dir)
+
+#for each receiver print pairs of fish seen 10 min form each other
+#10 min is 0.2 epoch ?
+#salmomiks.check_fish_pairs(fish_file)
+
+fish_file = 'data_badger_thresh2epoch/data_thresh2epoch/fish_moove.csv'
+salmomiks.check_fish_pairs(fish_file, rec_file_better, pairs_map_file)
--- salmomiks.py
+++ salmomiks.py
@@ -6,16 +6,19 @@ import numpy as np
 import matplotlib as mpl
 import matplotlib.pyplot as plt
 from matplotlib import cm
+from itertools import cycle
 import seaborn as sns
 
 import dateutil
 import folium
 import pyproj
 
+
 def load_data(recievers_list_file
               , tag_list_file
               , observations_filelist
               , filtered_data_file
+              , rec_better_data_file # list of recievers with downstream distance
 ):
 
     #TODO make sure correct results on different systems
@@ -98,6 +101,7 @@ def load_data(recievers_list_file
     print(df_observations.sample(30))
     #Adding distance from first reciever
     first_rec_id=480432
+    opposite_dir_id=483479 # this is a reciever at the wrong end of a lake
     rec_list = df_observations.drop_duplicates(['rec_id']).set_index('rec_id')
     rec_list = rec_list.dropna()
     print(rec_list)
@@ -111,6 +115,9 @@ def load_data(recievers_list_file
                                 , rec_list.loc[first_rec_id]['lat']
                                 , rec['lon']
                                 , rec['lat'])
+        if index == opposite_dir_id:
+            dist = - dist
+
         dists[index]=int(dist)
 
     rec_list['dist_first']=pd.Series(dists)
@@ -125,6 +132,7 @@ def load_data(recievers_list_file
 
     print('Saving observations to filtered.csv')
     df_observations.to_csv(filtered_data_file)
+    rec_list.to_csv(rec_better_data_file)
 
     return None
 
@@ -142,7 +150,7 @@ def make_rec_map(filtered_file, map_file):
         fishes = fish_counts[index]
 
         folium.CircleMarker(location = [rec['lat'], rec['lon']]
-                            , popup=str(index)
+                            , popup=str(index) + ' at distance ' + str(rec['dist_first'])
                             , color = colour
                             , radius=np.sqrt(fishes)/10
                             , fill=True
@@ -196,6 +204,8 @@ def make_fish_corr(filtered_file):
         for idy, ty in enumerate(tags):
             fy = filtered_list[filtered_list['tags']==ty].set_index('epoch')
             correg[idx,idy] =  fx['dist_first'].corr(fy['dist_first'])
+            if correg[idx,idy] > 0.7 and tx != ty:
+                print('Check ' + str(tx) + ' and ' + str(ty) + ', with correlation ' + str(correg[idx,idy]))
 
 
     ###############PRINT MAIN MAP
@@ -281,6 +291,85 @@ def check_fish_coocurrence(fish_file, rec_groups_dir):
         dfs.to_csv(rec_groups_dir + str(key) + '.csv')
         #plt.plot(dfs.groupby('shoals').size())
         #plt.show()
+
+def check_fish_pairs(fish_file, rec_better_file, pairs_map_file):
+    ff = pd.read_csv(fish_file)
+
+    #for each receiver
+    timethreshold = 2 #how many epochs (epoch is 0.1 of an hour, i.e. 6 min)
+    dfss=dict()
+
+    for d in ff['dist_first'].unique(): #for each distance, so for each receiver
+        df = ff[ff['dist_first']==d][['datetime','epoch','dist_first','tags']]
+        df = df.sort_values(by=['epoch']).reset_index(drop=True)
+
+        df['fishdiff']= ~(df['tags'] == df['tags'].shift())
+        df['mfishdiff']= ~(df['tags'].shift(-1) == df['tags'])
+
+        df['tdiff']=df['epoch'] - df['epoch'].shift(1, fill_value=0)
+        df['mtdiff']=df['epoch'].shift(-1, fill_value=0) - df['epoch']
+
+        dffilt = df[
+            ((df['tdiff']<timethreshold) & (df['fishdiff'])) | ((df['mtdiff']<timethreshold)  & (df['mfishdiff']))
+        ]
+
+        dfss[d]=dffilt
+
+    unique_fishpairs = dict()
+    #give clusters a name
+    for key, dffilt in dfss.items():
+        all_pairs_list = [] #those two could be sets as I turn them to sets anyway
+        pairs_list = []
+        for ix, valx in dffilt.iterrows():
+            if valx['tdiff']>=timethreshold: #end of a shoal
+                if len(pairs_list) == 0: #no fish in a buffer
+                    pairs_list.append(valx['tags'])
+                continue
+            pairs_list.append(valx['tags'])
+            if len(frozenset(pairs_list))==2:#list of two identical ID can be length 2
+                all_pairs_list.append(frozenset(pairs_list)) #set is unordered. Frozenset is because python likes the eternal winter of the mind
+                pairs_list = []
+        print(str(key))
+        #print( "all_pairs_list") # set only has unique elements
+        #print( all_pairs_list) # set only has unique elements
+        #print( set(all_pairs_list)) # set only has unique elements
+        unique_fishpairs[key] = set(all_pairs_list)
+
+    print("comparison")
+    bygone_keys = []
+    pairs = dict()
+    for key1, val1 in unique_fishpairs.items():
+        pairs[key1]=[]
+        for key2, val2 in unique_fishpairs.items():
+            if key1 == key2 or key2 in bygone_keys:
+                continue
+            if(len(val1.intersection(val2))!=0):
+                print('Found it, yes!!!! There the same lovely couple of salmon at the receiver ' + str(key1) + ' and ' + str(key2) + ' :D ')
+                print(val1.intersection(val2))
+                pairs[key1].append(val1.intersection(val2))
+        bygone_keys.append(key1)
+
+    rec_list = pd.read_csv(rec_better_file).set_index('dist_first')
+
+    m = folium.Map(location=[57.0890, -4.7483], zoom_start=15)
+
+    cycol = cycle('bgrcmk')
+    for index, rec in rec_list.iterrows():
+        fishes_pairs = len(pairs[abs(index)]) #HACK TODO TMP becasue I use data crunched before
+        colour = next(cycol)
+        folium.CircleMarker(location = [rec['lat'], rec['lon']]
+                            , popup=str(index) + ' at distance ' + str(rec['rec_id'])
+                            , color = colour
+                            , radius= 1 + np.sqrt(fishes_pairs)
+                            , fill=True
+                            , fill_color=colour
+                            , stroke = True
+                            , weight=1
+                            , fill_opacity=.55
+        ).add_to(m)
+        m.save(pairs_map_file)
+
+
 
 
 
