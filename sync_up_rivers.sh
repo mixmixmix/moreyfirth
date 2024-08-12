6234c56b2ea899a0f5ae7662a7f7c09dbf7479a1
M Kunde <mikolaj.kundegorski@gmail.com>
2024-08-12 14:18:18 +0100

    sync up euclid

--- sync_up_rivers.sh
+++ sync_up_rivers.sh
@@ -6,6 +6,7 @@ ping -c 2 -W 2 $badger_ip
 if [ $? -eq 0 ]; then
   echo "Hello Badgooo!!"
   rsync --update --partial --progress -rchv -e "ssh" out/ badger:/home/mix/repos/phdeep/smolts-ibm-modelling/experiments/real_rivers/
+  rsync --update --partial --progress -rchv -e "ssh" out/ euclid35:/home/pgrad1/2412135k/smolts-ibm-modelling/experiments/real_rivers/
 else
   echo "Unable to reach badger. Rsync aborted."
 fi
